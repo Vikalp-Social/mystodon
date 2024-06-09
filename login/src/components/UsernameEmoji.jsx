1df4a59a5785efab4a00cf49d58677a2c232e0cb
DharunThota <dharunth@gmail.com>
2024-06-09 15:56:52 +0530

    render multiple emojis

--- login/src/components/UsernameEmoji.jsx
+++ login/src/components/UsernameEmoji.jsx
@@ -3,22 +3,23 @@ import React, { useEffect, useState } from 'react'
 function UsernameEmoji(props) {
 	const [emojis, setEmojis] = useState([])
 	const [name, setName] = useState(props.name)
-
-	console.log(props)
+	var temp = props.name;
 
 	useEffect(() => {
-		emoji(props.name)	
-	}, [props.name, props.emojis]);
+		emoji(props.name);
+	}, [props.name]);
 
 	function emoji(str){
-        var emojis = /(?<=:)\w+(?=:)/g.exec(str);
-        if(emojis && props.emojis.length){
-            emojis.map(emoji => {
-                let custom_emoji = props.emojis.find((value) => value.shortcode === emoji)
-                setName(() => str.replace(`:${emoji}:`, ""))
-				setEmojis(prev => [...prev, custom_emoji.url])
-            })
-        }
+		if(props.emojis && props.emojis.length){
+			var t = str
+			props.emojis.map(emoji => {
+				if(t.search(emoji.shortcode)){
+					t = t.replace(`:${emoji.shortcode}:`, "")
+					setName(t)
+					setEmojis(prev => [...prev, emoji.url])
+				}
+			})
+		}
     }
 
 	return (
