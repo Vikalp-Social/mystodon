b2760c5271b577e0c36fbaa132b93d1627bd88d3
DharunThota <dharunth@gmail.com>
2024-11-10 16:41:23 +0530

    update for local development

--- src/apis/APIClient.js
+++ src/apis/APIClient.js
@@ -1,31 +1,24 @@
 import axios from "axios";
 
-const urls = {
-    1: "https://tov7mk1km9.execute-api.eu-north-1.amazonaws.com/stage1",  // URL for server 1
-    2: "https://sentiment.srg.social",    // URL for server 2
-};
-
-// const ports = {
-//     1: 3000,
-//     2: 5000,
-// }
+const ports = {
+    1: 3000,
+    2: 5000,
+}
 
 const selectedServer = localStorage.getItem("server");
-//const port = ports[selectedServer];
+const port = ports[selectedServer];
 
 if(!selectedServer){
     localStorage.setItem("server", 1);
     window.location.reload(false);
 }
-else if(!(selectedServer in urls)){
+else if(!(selectedServer in ports)){
     alert("Invalid Server Selected. Redirecting to Default Server");
     localStorage.setItem("server", 1);
     window.location.reload(false);
 }
 
-const baseURL = urls[selectedServer];
-
 // Created a new instance of axios with the dynamically selected baseURL
 export default axios.create({
-    baseURL: `${baseURL}/api/v1/`,
+    baseURL: `http://localhost:${port}/api/v1/`,
 });
