885b27c17c2d67e81e39cce3561dcc6a05f00532
SRG <itsmesrg@gmail.com>
2024-11-16 01:05:40 +0530

    text and css changes

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -12,7 +12,7 @@ function About() {
       <header className="about-header">
         <h1>Welcome to Vikalp.Social</h1>
         <p>
-          Join a network that values privacy, autonomy, and community. Here, you’re part of a user-first platform.
+          Choose Your Algorithms, Choose Your Experiences.
         </p>
         <div className="cta-buttons">
           <button className="my-button" type="submit" onClick={() => window.location.pathname = "/"}>Explore</button>
@@ -24,9 +24,8 @@ function About() {
         <h2>Our Mission: Freedom of Choice</h2>
         <p>
           <strong>Vikalp</strong>, which means “Choice” in various Indian languages, represents our vision: 
-          empowering users with the freedom to decide what they consume on social media and how they consume it. 
-          At Vikalp.Social, we are dedicated to providing options that put you in control.
-        </p>
+          empowering users with the freedom to decide what they consume on social media and how they consume it.</p>
+          <p>At Vikalp.Social, we are dedicated to providing options that put you in control.</p>
         <p>
           We’re a work in progress, continuously improving and evolving to create a platform that gives people the 
           freedom to shape their social media experience.
@@ -35,21 +34,21 @@ function About() {
 
       <section className="about-features">
         <div className="feature">
-          <h2>Decentralized and Open-Source</h2>
+          <h2>Part of the Fediverse</h2>
           <p>
-            Your data remains yours, free from centralized control. Join a network by the users, for the users.
+            Vikalp is just a client which can connect to any Mastodon Instance.
           </p>
         </div>
         <div className="feature">
-          <h2>No Ads, No Tracking</h2>
+          <h2>Privacy First</h2>
           <p>
-            Enjoy a space free from ads, tracking, and hidden algorithms. Connect and engage without distractions.
+          We do not store any information about you. Enjoy a space free from tracking, and hidden algorithms.
           </p>
         </div>
         <div className="feature">
-          <h2>Community-Focused Connections</h2>
+          <h2>100% Open Source</h2>
           <p>
-            Explore and join communities that matter to you. Unique, user-moderated spaces designed for genuine engagement.
+            Open Source is part of our DNA. Find all the code <a href='https://github.com/Vikalp-Social'>here</a>.
           </p>
         </div>
       </section>
